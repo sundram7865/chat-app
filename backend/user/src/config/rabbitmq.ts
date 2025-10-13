268186dc92ac04781e63a8725e3bac6f1f9009b0
sundram <ram322222@gmail.com>
2025-10-13 13:58:58 +0530

    setup rabbit mq

---
+++ backend/user/src/config/rabbitmq.ts
@@ -0,0 +1,30 @@
+import ampq from "amqplib";
+
+let channel:ampq.Channel
+
+export const connectRabbitMQ=async()=>{
+    try {
+        const connection=await ampq.connect({
+             protocol: "amqp",
+             hostname: process.env.RABBITMQ_HOST,
+             port: 5672,
+             username: process.env.RABBITMQ_USER,
+             password: process.env.RABBITMQ_PASSWORD,
+        });
+       channel = await connection.createChannel();
+       console.log('RabbitMQ connected')
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+export const publishTOQueue=async(queueName:string,message:any)=>{
+    if(!channel){
+        console.log('RabbitMQ is not initialized');
+        return;
+    }
+    await channel.assertQueue(queueName,{durable:true});
+    await channel.sendToQueue(queueName,Buffer.from(JSON.stringify(message)),{
+        persistent:true
+    });
+}
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import {createClient} from 'redis'
+import  userRoutes from './routes/user.js';
+import { connectRabbitMQ } from './config/rabbitmq.js';
 dotenv.config();
 connectDB();
-
+connectRabbitMQ();
 export const redisClient = createClient({
     url: process.env.REDIS_URI as string,
 });
@@ -13,7 +15,7 @@ redisClient.connect()
 .catch(console.error);
 
 const app = express();
-
+app.use("api/v1",userRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
