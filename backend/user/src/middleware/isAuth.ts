edd348fbae8d0cb808a9f020b90344dba283e55f
sundram <ram322222@gmail.com>
2025-10-14 10:21:35 +0530

    craeted jwt based auth

---
+++ backend/user/src/middleware/isAuth.ts
@@ -0,0 +1,40 @@
+import type { IUser } from "../model/user.js";
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import { User } from "../model/user.js";
+export interface AuthenticatedRequest extends Request {
+        user?: IUser | null;
+  }
+
+export const isAuth = async (req: AuthenticatedRequest, res: Response, next: NextFunction):
+Promise<void> => {
+    try{
+        const authHeader = req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith('Bearer ')){
+         res.status(401).json({message:"Unauthorized"});   
+         return;
+        }
+        const token = authHeader.split(' ')[1];
+        if (!token) {
+           res.status(401).json({ message: 'Token missing' });
+           return;
+        }
+        const decodedValue = jwt.verify(token,process.env.JWT_SECRET as string) as JwtPayload;
+        if (!decodedValue || !decodedValue.user) {
+            res.status(401).json({ message: "Unauthorized" });
+            return;
+        }
+        req.user = await User.findById(decodedValue.user);
+        next();
+    }catch(error:any){
+        res.status(401).json({
+            message:"please login -JWT ERROR"
+        })
+    }
+}
+
+
+export  const myProfile=(req:AuthenticatedRequest,res:Response)=>{
+    const user=req.user;
+    res.json(user);
+}
--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,8 +1,10 @@
 import express  from "express";
 import { loginUser, verifyUser } from "../controllers/user.js";
+import { isAuth, myProfile } from "../middleware/isAuth.js";
 const router = express.Router();
 
 
 router.post('/login', loginUser);
 router.post('/verify', verifyUser);
+router.get('/me',isAuth,myProfile);
 export default router
