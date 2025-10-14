dec00270f0515f21e183d01790c01479045c4f04
sundram <ram322222@gmail.com>
2025-10-14 15:35:14 +0530

    created message model

--- backend/chat/src/controllers/chat.ts
+++ backend/chat/src/controllers/chat.ts
@@ -22,3 +22,16 @@ export const createNewChat = TryCatch(async (req: AuthenticatedRequest, res) => 
          chatId:newChat._id
     });
 });
+
+export const getAllChats = TryCatch(async (req: AuthenticatedRequest, res) => {
+    const userId = req.user?._id;
+    if(!userId) return res.status(401).json({message:"Unauthorized user-id missing"});
+    const chats = await Chat.find({ users: userId }).sort({updatedAt:-1});
+
+    const chatWithUserData = await Promise.all(
+        chats.map(async (chat) => {
+            const otherUserId = chat.users.find((id) => id !== userId);
+        })
+    )
+       
+})
